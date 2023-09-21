e77a3d8903fbfe0db0fed067a3cf92544e1343ac
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2023-09-21 15:37:16 -0400

    database connected, tables created, data displayed through react page

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost/fullstack_template_db');
+const client = new pg.Client('postgres://localhost/movies_db');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -16,13 +16,35 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
 
-const init = async()=> {
+app.get('/api/movies', async (req, res, next) => {
+  try {
+    const SQL = `
+      SELECT *
+      FROM movies;
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch(error) {
+    next(error)
+  }
+});
+
+const init = async ()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
+    DROP TABLE IF EXISTS movies;
+    CREATE TABLE movies(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255),
+      stars INTEGER
+    );
+    INSERT INTO movies (name, stars) VALUES ('Fast and Furious', 5);
+    INSERT INTO movies (name, stars) VALUES ('Avengers', 5);
+    INSERT INTO movies (name, stars) VALUES ('Transformers', 0);
   `;
   console.log('create your tables and seed data');
+  await client.query(SQL);
 
   const port = process.env.PORT || 3000;
   app.listen(port, ()=> {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
-const App = ()=> {
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await axios.get('/api/movies');
+      setMovies(response.data);
+    }
+    fetchMovies();
+  }, [])
+
   return (
-    <h1>FullStack Template</h1>
+    <div>
+      <h1>Movies Rater ({movies.length})</h1>
+      <ul>
+        {
+          movies.map(movie => {
+            return (
+              <li key={movie.id}>
+                {`${movie.name} - ${movie.stars} stars `}
+                <button>+</button>
+                <button>-</button>
+              </li>
+            );
+          })
+        }
+      </ul>
+    </div>
   );
 };
 
